782cf30b05753f5b981129426a6f273bde7ed1ee
Amanda Cezario <amanda.cezariom@outlook.com>
2025-09-14 08:20:33 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,25 @@
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Detalhes from "./pages/Detalhes";
+
+function App() {
+  return (
+    <div className="bg-black min-h-screen text-white">
+      <Router>
+        <header className="p-4 bg-gray-900 shadow-md">
+          <h1 className="text-2xl font-bold">🎬 App de Filmes</h1>
+        </header>
+
+        <main className="p-6">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/detalhes/:id" element={<Detalhes />} />
+          </Routes>
+        </main>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
